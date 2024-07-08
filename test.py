11693e54d56c67b3f52677eb7acce0b88edd4ae0
joonavel <joonavel@gmail.com>
2024-07-08 11:50:46 +0900

    black test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-def greet(name):
-    return 'Hello, ' + name # Bad formatting and missing type annotations
+def greet(name: str) -> str:
+    return "Hello, " + name
+
 
 if __name__ == "__main__":
-    print(greet('World'))
+    print(greet("World"))
